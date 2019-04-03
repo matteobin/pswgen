3132af69b03814506e82362085edcfcbd2e04b1e
Matteo Bini <matteobin@protonmail.com>
2019-04-03 15:30:25 +0200

    Added case insensitive option and rewrote core in sheer C.

---
+++ Linux/pswgen.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+static int getRandNumBtwn(int min, int max) {
+	int range = max - min + 1;
+	int remainder = RAND_MAX % range;
+	int randomNumber;
+	do {
+		randomNumber = rand();
+	} while (randomNumber >= RAND_MAX - remainder);
+	return min + randomNumber % range;
+}
+
+static void showHelp(char name[]) {
+	printf("Welcome to PaSsWord Generator 3.0! Coded by Matteo Bini.\nThis software creates alpha-numeric, pseudo-randomly chosen from 25 to 50 characters-long passwords.\n\nUsage: %s <option(s)>\nOptions:\n-h, --help\t\tshow help\n-l, --length NUMBER\tset password length\n-ci, --case-insensitive\tcreates a lowercase only password\n-c, --complex\t\tcreates an even more complex password, with special characters as well\nSpecial characters:\t !\"#$%%&'()*+,-./:;<=>?@[\\]^_`{|}~\n\nAfter running the command, all you have to do is copy and paste your new password wherever you need to.\n", name);
+}
+
+int main(int argc, char* argv[]) {	
+	int len = 0; 
+    int caseSens = 1;
+	int complex = 0;
+	char* arg;
+	for (int i=1; i<argc; ++i) {
+        strcpy(arg, argv[i]);
+        if (strcmp(arg, "-h")==0 || strcmp(arg, "--help")==0) {
+			showHelp(argv[0]);
+			return 0;
+        } else if (strcmp(arg, "-l")==0 || strcmp(arg, "--length")==0) {
+            if(i+1 < argc) {
+                sscanf(argv[++i], "%d", &len);
+                if (len==0) {
+                    printf("\nERROR: length option requires a valid number.\n");
+                }
+            } else {
+                printf("\nERROR: length option requires at least one number.\n");
+            }
+        }
+        else if (strcmp(arg, "-ci")==0 || strcmp(arg, "--case-insensitive")==0) {
+            caseSens = 0;
+        }
+        else if (strcmp(arg, "-c")==0 || strcmp(arg, "--complex")==0) {
+            complex = 1;
+        }
+    }
+    srand(time(NULL));
+    if (len==0) {
+        len = getRandNumBtwn(25, 50);
+    }
+    char availChars[98] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    int availCharsNum = 36;
+    if (caseSens) {
+        strcat(availChars, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        availCharsNum += 26; 
+    }
+	if (complex) {
+		strcat(availChars, " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+        availCharsNum += 35; 
+	}
+	char psw[len+1];
+    strcpy(psw, "");
+    char randChar[2]; 
+    int randCharInd;
+	for (int i=0; i<len; i++) {
+        randCharInd = getRandNumBtwn(0, availCharsNum-1);
+        strncpy(randChar, availChars+randCharInd, 1);
+        strcat(psw, randChar);
+	}
+    printf("\nYour password is: %s.\n", psw);
+    return 0;
+}
---
+++ Windows/PSWGen.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+static int getRandNumBtwn(int min, int max) {
+	int range = max - min + 1;
+	int remainder = RAND_MAX % range;
+	int randomNumber;
+	do {
+		randomNumber = rand();
+	} while (randomNumber >= RAND_MAX - remainder);
+	return min + randomNumber % range;
+}
+
+static void showHelp(char name[]) {
+	printf("Welcome to PaSsWord Generator 3.0! Coded by Matteo Bini.\nThis software creates alpha-numeric, pseudo-randomly chosen from 25 to 50 characters-long passwords\nand it automatically copies them to the system clipboard.\n\nUsage: %s <option(s)>\nOptions:\n-h, --help\t\tshow help\n-l, --length NUMBER\tset password length\n-ci, --case-insensitive\tcreates a lowercase only password\n-c, --complex\t\tcreates an even more complex password, with special characters as well\nSpecial characters:\t !\"#$%%&'()*+,-./:;<=>?@[\\]^_`{|}~\n\nAfter running the command, all you have to do is copy and paste your new password wherever you need to.\n", name);
+}
+
+int main(int argc, char* argv[]) {	
+	int len = 0; 
+    int caseSens = 1;
+	int complex = 0;
+	char* arg;
+	for (int i=1; i<argc; ++i) {
+        strcpy(arg, argv[i]);
+        if (strcmp(arg, "-h")==0 || strcmp(arg, "--help")==0) {
+			showHelp(argv[0]);
+			return 0;
+        } else if (strcmp(arg, "-l")==0 || strcmp(arg, "--length")==0) {
+            if(i+1 < argc) {
+                sscanf(argv[++i], "%d", &len);
+                if (len==0) {
+                    printf("\nERROR: length option requires a valid number.\n");
+                }
+            } else {
+                printf("\nERROR: length option requires at least one number.\n");
+            }
+        }
+        else if (strcmp(arg, "-ci")==0 || strcmp(arg, "--case-insensitive")==0) {
+            caseSens = 0;
+        }
+        else if (strcmp(arg, "-c")==0 || strcmp(arg, "--complex")==0) {
+            complex = 1;
+        }
+    }
+    srand(time(NULL));
+    if (len==0) {
+        len = getRandNumBtwn(25, 50);
+    }
+    char availChars[98] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    int availCharsNum = 36;
+    if (caseSens) {
+        strcat(availChars, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        availCharsNum += 26; 
+    }
+	if (complex) {
+		strcat(availChars, " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+        availCharsNum += 35; 
+	}
+    int pswArLen = len+1;
+	char psw[pswArLen];
+    strcpy(psw, "");
+    char randChar[2]; 
+    int randCharInd;
+	for (int i=0; i<len; i++) {
+        randCharInd = getRandNumBtwn(0, availCharsNum-1);
+        strncpy(randChar, availChars+randCharInd, 1);
+        strcat(psw, randChar);
+	}
+    printf("\nYour password is: %s.\n", psw);
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, pswArLen);
+	memcpy(GlobalLock(hMem), psw, pswArLen);
+	GlobalUnlock(hMem);
+	OpenClipboard(0);
+	EmptyClipboard();
+	SetClipboardData(CF_TEXT, hMem);
+	CloseClipboard();
+    return 0;
+}
