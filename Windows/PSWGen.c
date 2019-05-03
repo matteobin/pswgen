8890d324f5c15e5ed5f0f5be9c06ca231ebe9a26
Matteo Bini <matteobin@protonmail.com>
2019-05-03 12:28:44 +0200

    Added numbers only password option.

--- Windows/PSWGen.c
+++ Windows/PSWGen.c
@@ -15,11 +15,12 @@ int getRandNumBtwn(int min, int max) {
 }
 
 void showHelp(char name[]) {
-	printf("Welcome to PaSsWord Generator 3.0! Coded by Matteo Bini.\nThis software creates alpha-numeric, pseudo-randomly chosen from 25 to 50 characters-long passwords\nand it automatically copies them to the system clipboard.\n\nUsage: %s <option(s)>\nOptions:\n-h, --help\t\tshow help\n-l, --length NUMBER\tset password length\n-ci, --case-insensitive\tcreates a lowercase only password\n-c, --complex\t\tcreates an even more complex password, with special characters as well\nSpecial characters:\t !\"#$%%&'()*+,-./:;<=>?@[\\]^_`{|}~\n\nAfter running the command, all you have to do is copy and paste your new password wherever you need to.\n", name);
+	printf("Welcome to PaSsWord Generator 3.1! Coded by Matteo Bini.\nThis software creates alpha-numeric, pseudo-randomly chosen from 25 to 50 characters-long passwords.\n\nUsage: %s <option(s)>\nOptions:\n-h, --help\t\tshow help\n-l, --length NUMBER\tset password length\n-n, --numbers-only\tcreates a number only password\n-ci, --case-insensitive\tcreates a lowercase only password\n-c, --complex\t\tcreates an even more complex password, with special characters as well\nSpecial characters:\t !\"#$%%&'()*+,-./:;<=>?@[\\]^_`{|}~\n\nAfter running the command, all you have to do is copy and paste your new password wherever you need to.\n", name);
 }
 
 int main(int argc, char* argv[]) {	
 	int len = 0;
+    int numsOnly = 0;
 	int caseSens = 1;
 	int complex = 0;
 	char arg[19];
@@ -31,40 +32,46 @@ int main(int argc, char* argv[]) {
 		if (strcmp(arg, "-h")==0 || strcmp(arg, "--help")==0) {
 			showHelp(argv[0]);
 			return 0;
-		}
-		else if (strcmp(arg, "-l")==0 || strcmp(arg, "--length")==0) {
-			if (i + 1 < argc) {
-				sscanf(argv[++i], "%i", &len);
-				if (len<=0 || len>50) {
-					printf("\nERROR: length option requires a valid number between 1 and 50.\n");
-                    len = 0;
-				}
-			}
-			else {
-				printf("\nERROR: length option requires at least one number between 1 and 50.\n");
-			}
-		}
-		else if (strcmp(arg, "-ci")==0 || strcmp(arg, "--case-insensitive")==0) {
-			caseSens = 0;
-		}
-		else if (strcmp(arg, "-c")==0 || strcmp(arg, "--complex")==0) {
-			complex = 1;
+		} else {
+            if (strcmp(arg, "-l")==0 || strcmp(arg, "--length")==0) {
+                if (i+1<argc) {
+                    sscanf(argv[++i], "%i", &len);
+                    if (len<=0 || len>50) {
+                        printf("\nERROR: length option requires a valid number between 1 and 50.\n");
+                        len = 0;
+                    }
+                }
+                else {
+                    printf("\nERROR: length option requires at least one number between 1 and 50.\n");
+                }
+            }
+            if (strcmp(arg, "-n")==0 || strcmp(arg, "--numbers-only")==0) {
+                numsOnly = 1;
+            } else if (strcmp(arg, "-ci")==0 || strcmp(arg, "--case-insensitive")==0) {
+			    caseSens = 0;
+		    } else if (strcmp(arg, "-c")==0 || strcmp(arg, "--complex")==0) {
+			    complex = 1;
+		    }
 		}
 	}
 	srand(time(NULL));
 	if (len==0) {
 		len = getRandNumBtwn(25, 50);
 	}
-	char availChars[98] = "0123456789abcdefghijklmnopqrstuvwxyz";
-	int availCharsNum = 36;
-	if (caseSens) {
-		strcat(availChars, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-		availCharsNum += 26;
-	}
-	if (complex) {
-		strcat(availChars, " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
-		availCharsNum += 35;
-	}
+    char availChars[96] = "0123456789";
+	int availCharsNum = 10;
+    if (!numsOnly) {
+        strcat(availChars, "abcdefghijklmnopqrstuvwxyz");
+        availCharsNum += 26;
+        if (caseSens) {
+            strcat(availChars, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            availCharsNum += 26;
+        }
+        if (complex) {
+            strcat(availChars, " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+            availCharsNum += 33;
+        }
+    }
 	char psw[51] = "";
 	char randChar[2] = "";
 	int randCharInd;
